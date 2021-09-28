0be9c3f2298ef9898da667b096770963735a82c7
Alan <alan.mcfarlane@gmail.com>
2021-09-28 07:50:33 +0100

    changed storage defaults for local/public

--- config/storage.php
+++ config/storage.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 use App\Core;
 use League\Flysystem\Local\LocalFilesystemAdapter;
+use League\Flysystem\UnixVisibility\PortableVisibilityConverter;
+use League\Flysystem\Visibility;
 
 return [
 
@@ -10,6 +12,7 @@ return [
         'driver' => LocalFilesystemAdapter::class,
         'params' => [
             'location' => Core::path('storage/app'),
+            'visibility' => PortableVisibilityConverter::fromArray([], Visibility::PUBLIC),
         ],
     ],
 
@@ -17,7 +20,7 @@ return [
         'driver' => LocalFilesystemAdapter::class,
         'params' => [
             'location' => Core::path('storage/app/public'),
+            'visibility' => PortableVisibilityConverter::fromArray([], Visibility::PUBLIC),
         ],
     ],
-
 ];
