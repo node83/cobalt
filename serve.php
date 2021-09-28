600f47c725d934ceb267074b6c6fd8ae7e8d18bd
Alan <alan.mcfarlane@gmail.com>
2021-09-28 22:59:47 +0100

    added woff2 to supported files

--- serve.php
+++ serve.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-if ((PHP_SAPI === 'cli-server') && preg_match('`\.(css|gif|ico|jpeg|jpg|js|map|png)$`i',
+if ((PHP_SAPI === 'cli-server') && preg_match('`\.(css|gif|ico|jpeg|jpg|js|map|png|woff2)$`i',
         parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
     return false;
 }
